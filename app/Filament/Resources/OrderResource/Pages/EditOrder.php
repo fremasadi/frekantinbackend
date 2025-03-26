a2236d7460df6d1f7973fe5a07d2d9946ab29bd5
fremasadi <fremasadii@gmail.com>
2025-03-26 20:31:26 +0700

    feat:order item in orders

--- app/Filament/Resources/OrderResource/Pages/EditOrder.php
+++ app/Filament/Resources/OrderResource/Pages/EditOrder.php
@@ -15,17 +15,34 @@ class EditOrder extends EditRecord
         return 'Detail Order';
     }
 
-
     protected function getHeaderActions(): array
     {
         return [];
     }
 
-
+    // Solusi 1: Nonaktifkan seluruh save actions
     protected function hasSaveActions(): bool
     {
         return false;
     }
 
-    
+    // Solusi 2: Kosongkan form actions (lebih eksplisit)
+    protected function getFormActions(): array
+    {
+        return [];
+    }
+
+    // Solusi 3: Kombinasi keduanya untuk memastikan
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(static::getResource()::form(
+                $this->makeForm()
+                    ->model($this->getRecord())
+                    ->statePath('data')
+                    ->operation('edit')
+                    ->disabled() // Menonaktifkan seluruh form
+            )),
+        ];
+    }
 }
