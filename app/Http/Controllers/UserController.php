33728f6c1db9ba80c513c501385882d53ebcb61c
fremasadi <fremasadii@gmail.com>
2025-05-13 23:00:18 +0700

    fix generate absensi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserController extends Controller
 {
