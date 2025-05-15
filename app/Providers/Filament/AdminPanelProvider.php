252bab4b6a2f0e4e5e8764fc98c41bc51cb6db0e
fremasadi <fremasadii@gmail.com>
2025-05-15 18:39:14 +0700

    feat

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,9 +33,9 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => '#264F0B',
             ])
             ->brandName('Kantin Nguldi')
-            ->auth(fn () => Auth::check() && Auth::user()->role === 'admin')
             ->darkMode(false) 
             ->topNavigation(false)
+            ->auth(fn () => Auth::check() && Auth::user()->role === 'admin')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
