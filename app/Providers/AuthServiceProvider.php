96d990d2be6f9591115da093e5ec6e7b61b387bc
fremasadi <fremasadii@gmail.com>
2025-05-15 19:02:39 +0700

    feat

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Sanctum::authenticateAccessTokensUsing(function ($token, $isValid) {
+            // Selalu validasi token tanpa memeriksa waktu kadaluarsa
+            return $isValid;
+        });
     }
 }
