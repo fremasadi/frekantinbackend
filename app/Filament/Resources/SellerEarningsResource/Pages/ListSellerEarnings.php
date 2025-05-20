76ac4c3e089a30039e7ac10f7b85b8bb0cc342c2
fremasadi <fremasadii@gmail.com>
2025-05-20 14:13:07 +0700

    feat seller earning

--- app/Filament/Resources/SellerEarningsResource/Pages/ListSellerEarnings.php
+++ app/Filament/Resources/SellerEarningsResource/Pages/ListSellerEarnings.php
@@ -13,7 +13,7 @@ class ListSellerEarnings extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
